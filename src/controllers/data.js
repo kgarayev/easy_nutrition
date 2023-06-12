0aac5d33f3b334bdc3d324b1c8097db329516582
kgarayev <kenangarayev@gmail.com>
2023-06-12 14:38:24 +0100

    bugs fixed

--- src/controllers/data.js
+++ src/controllers/data.js
@@ -6,26 +6,27 @@ import { newApiData, loading } from "../store/nutritionSlice";
 
 // function to get data from the api
 export const getData = async (input) => {
-  // only a backup data
-  store.dispatch(newApiData(backupData));
+  try {
+    store.dispatch(loading(true));
 
-  // try {
-  //   store.dispatch(loading(true));
+    // only a backup data
+    store.dispatch(newApiData(backupData));
 
-  //   const { data } = await axios.get(
-  //     `https://api.api-ninjas.com/v1/nutrition?query=${input}`,
-  //     {
-  //       headers: {
-  //         "X-Api-Key": API_KEY,
-  //         "Content-Type": "application/json",
-  //       },
-  //     }
-  //   );
+    // real api data
+    // const { data } = await axios.get(
+    //   `https://api.api-ninjas.com/v1/nutrition?query=${input}`,
+    //   {
+    //     headers: {
+    //       "X-Api-Key": API_KEY,
+    //       "Content-Type": "application/json",
+    //     },
+    //   }
+    // );
 
-  //   store.dispatch(loading(false));
+    store.dispatch(loading(false));
 
-  //   store.dispatch(newApiData(data));
-  // } catch (error) {
-  //   console.log(error);
-  // }
+    // store.dispatch(newApiData(data));
+  } catch (error) {
+    console.log(error);
+  }
 };
